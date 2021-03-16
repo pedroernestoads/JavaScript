e332325627f1eebb331d7976292cf902bcdc12d3
pedroernestoads <souza.pe@gmail.com>
2021-03-15 22:54:42 -0300

    Adicionado

--- aula3/index.js
+++ aula3/index.js
@@ -4,3 +4,5 @@ console.log(`Pedro`);
 
 console.log(123456, 15665132.123456, 'Pedro Ernesto');
 console.log(123456, 15665132.123456, 'Pedro Ernesto');
+
+console.log('Meu nome é "Pedro Ernesto". Estou aprendendo JavaScript às', 10, 'da manhã');
